2b017c4f40f6976288dd99909721ce3473bf3600
Lonkulus <redmanbad951@gmail.com>
2024-07-24 18:29:11 -0600

    Add files via upload

---
+++ FirstPersonTestGame/Bottle.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+
+@onready var _bottle_anim = $AnimatedSprite3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Bottle animation
+	_bottle_anim.play("bottle")
